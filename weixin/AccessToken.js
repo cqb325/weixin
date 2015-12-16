c5b3b8527e2643d00fc5a578fa4583f77c85b7f7
cqb325 <cqb325@163.com>
2015-12-16 23:00:37 +0800

    add getJsSDKTicket

--- weixin/AccessToken.js
+++ weixin/AccessToken.js
@@ -7,6 +7,8 @@ const APPSECRET = "d4624c36b6795d1d99dcf0547af5443d";
 const ACCESS_TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+APPID+"&secret="+APPSECRET;
 const ACCESS_TOKEN_FILE = __dirname + "/../conf/accessToken";
 
+var WXCONFIG = require("../config").WX;
+
 var fs = require("fs"),
     FileUtil = require("./FileUtil"),
     HttpsUtil = require("./HttpsUtil"),
@@ -23,7 +25,7 @@ module.exports = {
                 token = token ? JSON.parse(token.trim()) : token;
                 if(token){
                     //没过期直接使用
-                    if(self.checkValidate(token.access_token)){
+                    if(self.checkValidate(token)){
                         callback.call(scope, token.access_token);
                     }else{
                         //过期重新获取
@@ -73,5 +75,66 @@ module.exports = {
                 callback.call(scope, null);
             }
         }, null);
+    },
+
+    /**
+     * 获取JS API Ticket
+     * @param callback
+     * @param scope
+     */
+    getJsSDKTicket: function(callback, scope){
+        var fileURI = __dirname + "/.."+ WXCONFIG.JS_SDK_TICKET_FILE;
+        var self = this;
+        fs.exists(fileURI, function(exists){
+            if(exists){
+                var ticket = FileUtil.readFile(fileURI);
+                ticket = ticket ? JSON.parse(ticket.trim()) : ticket;
+                if(ticket){
+                    //没过期直接使用
+                    if(self.checkValidate(ticket)){
+                        callback.call(scope, ticket.ticket);
+                    }else{
+                        //过期重新获取
+                        self.reloadJSSDKTicket(callback, scope);
+                    }
+                }else{
+                    self.reloadJSSDKTicket(callback, scope);
+                }
+            }else{
+                self.reloadJSSDKTicket(callback, scope);
+            }
+        });
+    },
+
+    /**
+     * 重新加载js api ticket
+     * @param callback
+     * @param scope
+     */
+    reloadJSSDKTicket: function(callback, scope){
+        var fileURI = __dirname + "/.."+ WXCONFIG.JS_SDK_TICKET_FILE;
+        this.getAccessToken(function(token){
+            if(token){
+                var url = WXCONFIG.JS_SDK_TICKET_URL.replace("ACCESS_TOKEN", token);
+                HttpsUtil.httpsPostData(url, "", "GET", function(ret){
+                    if(ret){
+                        var json = JSON.parse(ret);
+                        if(json.ticket){
+                            var now = new Date();
+                            now.setMinutes(now.getMinutes() + json.expires_in);
+                            json.time = now;
+                            FileUtil.writeFile(fileURI, JSON.stringify(json));
+                            callback.call(scope, json.ticket);
+                        }else{
+                            callback.call(scope, null);
+                        }
+                    }else{
+                        callback.call(scope, null);
+                    }
+                }, null);
+            }else{
+                callback.call(scope, null);
+            }
+        }, this);
     }
 };
